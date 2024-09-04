b29f56f53d28beb2e80fd2b97159c13d173c1cd5
M-Eleoterio <m.eleoteriovieira@gmail.com>
2024-09-03 21:57:18 -0300

    :style add style to token creation page

--- module_B/solution/resources/views/token/create.blade.php
+++ module_B/solution/resources/views/token/create.blade.php
@@ -3,18 +3,21 @@
 @section('title', 'New token')
 @section('content')
 
-<form action={{ route('token.create') }} method="post">
-    @csrf
-    <input type="text" name="name" placeholder="Token name">
-    <select name="workspace_id">
-        @foreach ($workspaces as $workspace)
-            <option value={{$workspace['id']}}>{{$workspace['title']}}</option>
-        @endforeach
-    </select>
-    <button type="submit">Create</button>
-</form>
+<div id="new-token-container">
+    <h1>New Token</h1>
+    <form action={{ route('token.create') }} method="post" id="new-token-form">
+        @csrf
+        <input type="text" name="name" placeholder="Token name">
+        <select name="workspace_id">
+            @foreach ($workspaces as $workspace)
+                <option value={{$workspace['id']}}>{{$workspace['title']}}</option>
+            @endforeach
+        </select>
+        <button type="submit">Create</button>
+    </form>
 
-@if(session('message'))
-    <p style="color:red;">{{session('message')}}</p>
-@endif
+    @if(session('message'))
+        <p style="color:red;">{{session('message')}}</p>
+    @endif
+</div>
 @endsection
--- module_B/solution/resources/views/workspace/show.blade.php
+++ module_B/solution/resources/views/workspace/show.blade.php
@@ -2,23 +2,25 @@
 @section('title', $workspace['title'])
 @section('content')
 <h1>Workspace - {{$workspace['title']}}</h1>
-@foreach ($tokens as $token)
-    <div class="token">
-        <h2>{{ $token['name'] }} |
+<a id="new-token" href={{ route('token.create') }}>New Token</a>
+<div id="tokens">
+    @foreach ($tokens as $token)
+        <div class="token">
+            <h2>{{ $token['name'] }} |
+                @if ($token['revoked'])
+                    Revoked
+                @else
+                    Active
+                @endif
+            </h2>
+            <p>Created at {{ $token['created_at'] }}</p>
             @if ($token['revoked'])
-                Revoked
-            @else
-                Active
+                <p>Revoked at {{ $token['revocation_date'] }}</p>
             @endif
-        </h2>
-        <p>Created at {{ $token['created_at'] }}</p>
-        @if ($token['revoked'])
-            <p>Revoked at {{ $token['revocation_date'] }}</p>
-        @endif
-        @if (!$token['revoked'])
-            <a href={{ route('token.revoke', ['id' => $token['id']]) }}>Revoke it</a>
-        @endif
-    </div>
-@endforeach
-<a href={{ route('token.create') }}>New Token</a>
+            @if (!$token['revoked'])
+                <a href={{ route('token.revoke', ['id' => $token['id']]) }}>Revoke it</a>
+            @endif
+        </div>
+    @endforeach
+</div>
 @endsection
